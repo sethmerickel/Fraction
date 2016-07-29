643ccb684c4327eef7f9540fcfc588dc450474b6
Seth <sethmerickel@gmail.com>
2016-07-28 23:38:20 -0700

    Finished initial implementation of Fraction class

--- Fraction/Fraction.h
+++ Fraction/Fraction.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include <ratio>
+#include <iostream>
+
+// Helper functions
 
 // Greatest Common Divisor
 inline int gcd(int a, int b)
@@ -16,15 +18,158 @@ inline int gcd(int a, int b)
    return b;
 }
 
+//--------------------------------------------------------------------------
+
+inline int sign(int a)
+{
+   return a < 0 ? -1 : 1; // zero is +1
+}
+
+//--------------------------------------------------------------------------
+// Reduce a fraction.  Sign goes in numerator
+inline void reduce(int& num, int& den)
+{
+   int g = gcd(num, den);
+   num = sign(num) * sign(den) * std::abs(num) / g;
+   den = std::abs(den) / g;
+}
+
+// Fraction class
+// Invariants: 
+//     1) Should always be reduced
+//     2) Denominator should never be zero
+//     3) Numerator should always carry the sign
+
 class Fraction
 {
 public:
-	 
 
-   int getNumerator() const;
-   int getDenominator() const;
+   //--------------------------------------------------------------------------
+
+   Fraction()
+      :m_num(0), m_den(1)
+   {}
+   
+   //--------------------------------------------------------------------------
+
+   Fraction(int num, int den = 1)
+      :m_num(num), m_den(den)
+   {      
+      reduce(m_num, m_den);
+   }
+
+   //--------------------------------------------------------------------------
+
+   Fraction& operator=(const Fraction& f) = default;
+
+   //--------------------------------------------------------------------------
+
+   int getNumerator() const 
+   {
+      return m_num; 
+   }
+
+   //--------------------------------------------------------------------------
+
+   int getDenominator() const 
+   {
+      return m_den; 
+   }
 
 private:
-   int m_num;
-   int m_den;
+
+   // Members
+   int m_num; // Numerator
+   int m_den; // Denominator
+
+   // Friends
+
+   // Compare
+   friend bool operator==(const Fraction& f1, const Fraction& f2);
+   friend bool operator!=(const Fraction& f1, const Fraction& f2);
+   friend bool operator<(const Fraction& f1, const Fraction& f2);
+   friend bool operator>(const Fraction& f1, const Fraction& f2);
+
+   // Math
+   friend Fraction operator+(const Fraction& f1, const Fraction& f2);
+   friend Fraction operator-(const Fraction& f1, const Fraction& f2);
+   friend Fraction operator*(const Fraction& f1, const Fraction& f2);
+   friend Fraction operator/(const Fraction& f1, const Fraction& f2);
+
+   // Display
+   friend std::ostream& operator<<(std::ostream& os, const Fraction& f);
 };
+
+//--------------------------------------------------------------------------
+
+inline bool operator==(const Fraction& f1, const Fraction& f2)
+{
+   return (f1.m_num == f2.m_num) && (f1.m_den == f2.m_den);
+}
+
+//--------------------------------------------------------------------------
+
+inline bool operator!=(const Fraction & f1, const Fraction & f2)
+{
+   return !(f1 == f2);
+}
+
+//--------------------------------------------------------------------------
+
+inline bool operator<(const Fraction& f1, const Fraction& f2)
+{
+   Fraction diff = f1 - f2;
+   return diff.m_num < 0;
+}
+
+//--------------------------------------------------------------------------
+
+inline bool operator>(const Fraction& f1, const Fraction& f2)
+{
+   Fraction diff = f1 - f2;
+   return diff.m_num > 0;
+}
+
+//--------------------------------------------------------------------------
+
+inline Fraction operator+(const Fraction& f1, const Fraction& f2)
+{
+   int num = f1.m_num*f2.m_den + f2.m_num*f1.m_den;
+   int den = f1.m_den * f2.m_den;
+   return Fraction(num, den);
+}
+
+//--------------------------------------------------------------------------
+
+inline Fraction operator-(const Fraction& f1, const Fraction& f2)
+{
+   int num = f1.m_num*f2.m_den - f2.m_num*f1.m_den;
+   int den = f1.m_den * f2.m_den;
+   return Fraction(num, den);
+}
+
+//--------------------------------------------------------------------------
+
+inline Fraction operator*(const Fraction& f1, const Fraction& f2)
+{
+   int num = f1.m_num * f2.m_num;
+   int den = f1.m_den * f2.m_den;
+   return Fraction(num, den);
+}
+
+//--------------------------------------------------------------------------
+
+inline Fraction operator/(const Fraction& f1, const Fraction& f2)
+{
+   int num = f1.m_num * f2.m_den;
+   int den = f1.m_den * f2.m_num;
+   return Fraction(num, den);
+}
+
+inline std::ostream& operator<<(std::ostream& os, const Fraction& f)
+{
+   os << f.m_num << "/" << f.m_den;
+   return os;
+}
+
+//--------------------------------------------------------------------------
--- Fraction/Main.cpp
+++ Fraction/Main.cpp
@@ -13,6 +13,15 @@ void expect_eq(const T& expected, const T& result)
 }
 
 template <typename T>
+void expect_true(bool b)
+{
+   if (b == false)
+   {
+      std::cout << "FAILED: expression is false\n";
+   }
+}
+
+template <typename T>
 void expect_ne(const T& expected, const T& result)
 {
    if (expected == result)
@@ -30,6 +39,27 @@ int main()
    expect_eq(2, gcd(4, -2));
    expect_eq(5, gcd(0, 5));  // Not sure what this should return: 0, 1, or 5
    expect_eq(5, gcd(5, 0));  // Returning 5 seems to fit the definition of gcd the best
+
+   std::cout << "Test sign\n";
+   expect_eq(1, sign(1));
+   expect_eq(-1, sign(-4));
+   expect_eq(1, sign(0));
+
+   std::cout << "Test reduce\n";
+   int a = 2, b = 4;
+   expect_eq(a, 1); expect_eq(b, 2);
+   a = 0; b = 4;
+   reduce(a, b);
+   expect_eq(a, 0); expect_eq(b, 1);
+   a = 2; b = 0;
+   reduce(a, b);
+   expect_eq(a, 1); expect_eq(b, 0);
+   a = -2; b = 4;
+   reduce(a, b);
+   expect_eq(-1, a); expect_eq(2, b);
+   a = 2; b = -4;
+   reduce(a, b);
+   expect_eq(-1, a); expect_eq(2, b);
 
    return 0;
 }
